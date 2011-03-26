0820bdc593d383396a739ca370693d4b292283be
yvetterowe1116 <yvetterowe1116@d9104e88-05a6-3fce-0cda-3a6fd9c40cd8>
2011-03-26 03:37:25 +0000

    windowed ui 基本完成 利用自定义控件完成格子的模拟

---
+++ trunk/BombermanMapEditor/BombermanMapEditor/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BombermanMapEditor
+{
+    class Level
+    {
+        private List<Grid> grids;
+        private string levelName;
+        
+        public Level()
+        {
+        }
+    }
+}
---
+++ trunk/PaintBoard/PaintBoard/PaintBoard.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PaintBoard
+{
+    public partial class PaintBoard : Control
+    {
+        public int NumberOfRow
+        {
+            get;
+            set;
+        }
+
+        public int NumberOfCol
+        {
+            get;
+            set;
+        }
+
+        public delegate void ClickEventHandler(object sender, ClickEventArgs e);
+        public event ClickEventHandler OnClickGrid;
+
+        public class ClickEventArgs : EventArgs
+        {
+            public int Row
+            {
+                get;
+                set;
+            }
+
+            public int Col
+            {
+                get;
+                set;
+            }
+        }
+
+        public PaintBoard()
+        {
+            InitializeComponent();
+            NumberOfRow = 13;
+            NumberOfCol = 15;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Pen bp = new Pen(Color.Black, 2);
+            int sideLength = this.Height / this.NumberOfRow;
+
+            for (int i = 0; i < NumberOfRow+1; ++i)
+            {
+                Point beginH = new Point(0, sideLength*i);
+                Point endH = new Point(this.Width, sideLength*i);
+                pe.Graphics.DrawLine(bp, beginH, endH);
+            }
+
+            for (int j = 0; j < NumberOfCol+1; ++j)
+            {
+                Point beginV = new Point(sideLength* j, 0);
+                Point endV = new Point(sideLength* j, this.Height);
+                pe.Graphics.DrawLine(bp, beginV, endV);
+            }
+        }
+
+        private void PaintBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            int row = y / 20;
+            int col = x / 20;
+
+            ClickEventArgs ea = new ClickEventArgs();
+            ea.Row = row;
+            ea.Col = col;
+
+            if (OnClickGrid != null)
+            {
+                OnClickGrid(this, ea);
+            }
+        }
+
+        private void SetGridInfo()
+        {
+
+        }
+
+    }
+}

10978a2d495762c13ba947bebeb870ecaeab0f54
yvetterowe1116 <yvetterowe1116@d9104e88-05a6-3fce-0cda-3a6fd9c40cd8>
2011-03-27 09:51:28 +0000

    add interface for serializer/deserializer

---
+++ trunk/BombermanMapEditor/BombermanMapEditor/LevelSerialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+namespace BombermanMapEditor
+{
+    class LevelSerializer
+    {
+        public FileStream Serialize(Level level)
+        {
+            return null;
+        }
+
+        public Level Deserialize(FileStream stream)
+        {
+            return null;
+        }
+    }
+}

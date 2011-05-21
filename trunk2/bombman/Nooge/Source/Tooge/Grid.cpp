7fd009c59a003ca71f4661436532fd0c97804691
yvetterowe1116 <yvetterowe1116@d9104e88-05a6-3fce-0cda-3a6fd9c40cd8>
2011-05-21 02:27:39 +0000

    add Grid class

---
+++ trunk2/bombman/Nooge/Source/Tooge/Grid.cpp
@@ -0,0 +1,48 @@
+#include "Grid.h"
+
+Grid::Grid( float x1,float y1,float x2,float y2 )
+{
+	mX1 = x1;
+	mY1 = y1;
+	mX2 = x1;
+	mY2 = y2;
+}
+
+float Grid::X1()
+{
+	return mX1;
+}
+
+float Grid::Y1()
+{
+	return mY1;
+}
+
+float Grid::X2()
+{
+	return mX2;
+}
+
+float Grid::Y2()
+{
+	return mY2;
+}
+
+bool Grid::TestPoint( float x,float y )
+{
+	if(x<mX2 && x>mX1 && y<mY2 && y>mY1)
+		return true;
+	return false;
+}
+
+bool Grid::Intersect( Ref<Grid> grid )
+{
+	float x1 = grid->X1();
+	float x2 = grid->X2();
+	float y1 = grid->Y1();
+	float y2 = grid->Y2();
+
+	if(TestPoint(x1,y1) || TestPoint(x1,y2) || TestPoint(x2,y1) || TestPoint(x2,y2))
+		return true;
+	return false;
+}
---
+++ trunk2/bombman/Nooge/Source/Tooge/Grid.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Ref.h"
+
+class Grid
+{
+public:
+	Grid(float x1,float y1,float x2,float y2);
+
+	float X1();
+	float Y1();
+	float X2();
+	float Y2();
+
+	bool TestPoint(float x,float y);
+	bool Intersect(Ref<Grid> grid);
+private:
+	float mX1,mY1,mX2,mY2;
+};
